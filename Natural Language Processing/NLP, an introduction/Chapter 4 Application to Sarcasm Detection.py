ef4057304c724219353b95e4f3aa40e4cc8d7fcb
GOURAV PUJARI <60569217+wrangler-synthus@users.noreply.github.com>
2022-04-29 16:52:18 +0530

    Create Chapter 4 Application to Sarcasm Detection.py

---
+++ Natural Language Processing/NLP, an introduction/Chapter 4 Application to Sarcasm Detection.py
@@ -0,0 +1,31 @@
+# Sarcasm, Really?
+
+import json
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+with open('data/sarcasm.json', 'r') as f:
+    datastore = json.load(f)
+
+sentences  = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+tokenizer = Tokenizer(oov_token= '<OOV>')
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+
+print(len(word_index))
+sequences = tokenizer.texts_to_sequences(sentences)
+
+padded = pad_sequences(sequences, padding = 'post')
+
+print(sentences[0])
+print(padded[0])
+print(padded.shape)
